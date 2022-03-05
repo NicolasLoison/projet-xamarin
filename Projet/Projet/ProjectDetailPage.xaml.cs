aa14bd753398fc36be61174a460e4961f78281d0
Nicolas Loison <nicolas.loison45@gmail.com>
2022-03-05 15:59:48 +0100

    Ajout de la vue et du ViewModel pour le détail d'un projet

---
+++ Projet/Projet/ProjectDetailPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Projet
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ProjectDetailPage : ContentPage
+    {
+        private ProjectsViewModel _projectsViewModel;
+        private ProjectDetailViewModel _projectViewModel;
+        public ProjectDetailPage(ProjectsViewModel projectsViewModel, Model.Projet projet = null)
+        {
+            InitializeComponent();
+            _projectsViewModel = projectsViewModel;
+            _projectViewModel = new ProjectDetailViewModel(projectsViewModel, projet);
+            BindingContext = _projectViewModel;
+        }
+        
+        public void UpdateChosenTask(object sender, SelectedItemChangedEventArgs e)
+        {
+            _projectViewModel.TaskChoisie = e.SelectedItem as Model.Task;
+        }
+    }
+}
