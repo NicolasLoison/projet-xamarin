2df8b93b43031a63ad853b4450f09f3f6ec92500
nouailledegorcepaul <PNDikckdk72!!>
2022-03-07 16:15:37 +0100

    Merge des fichiers Nico en une seule Page pour Projet et Task

--- Projet/Projet/HomeViewModel.cs
+++ Projet/Projet/HomeViewModel.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.ObjectModel;
 using System.Windows.Input;
+using Plugin.Settings;
+using Plugin.Settings.Abstractions;
 using Projet.Model;
 using Storm.Mvvm;
 using Xamarin.Forms;
@@ -9,7 +12,17 @@ namespace Projet
     public class HomeModelView : ViewModelBase
     {
         // private string _nom, _prenom, _mail, _motDePasse;
-
+        private Model.Projet _chosenProject;
+        private ObservableCollection<Model.Projet> _projets = new ObservableCollection<Model.Projet>();
+        
+        static ISettings AppSettings => CrossSettings.Current;
+        public static string JsonProjects;
+        public static string SavedProjects 
+        {
+            get => AppSettings.GetValueOrDefault("MySettingKey", JsonProjects);
+            set => AppSettings.AddOrUpdateValue("MySettingKey", value);
+        }
+        
         public ICommand ProfileClick
         {
             get;
@@ -19,6 +32,17 @@ namespace Projet
         {
             get;
             set;
+        }
+        public ObservableCollection<Model.Projet> Projets
+        {
+            get => _projets;
+            set => SetProperty(ref _projets, value);
+        }
+        
+        public Model.Projet ChosenProject
+        {
+            get => _chosenProject;
+            set => SetProperty(ref _chosenProject, value);
         }
         
         public User User
@@ -30,6 +54,19 @@ namespace Projet
         public HomeModelView(User user)
         {
             User = user;
+            Model.Projet p = new Model.Projet(1, "Projet 1", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam et pretium sapien. Mauris ac justo eros. Nam accumsan ligula nunc, quis iaculis ligula feugiat a. Ut egestas nibh interdum sem porttitor, a vestibulum purus malesuada. Curabitur convallis, felis id mollis rhoncus, urna turpis ornare velit, nec consequat nisi lacus nec neque. Morbi varius faucibus metus sit amet vestibulum. Sed in varius tellus, ac imperdiet diam. Morbi iaculis leo enim, vel mattis risus tempus tincidunt. Fusce vitae felis dolor. Vestibulum rhoncus dapibus nisi vitae sagittis. Sed facilisis sit amet risus eu pharetra.");
+            Task t = new Task(1, "Ecrire le code à la ligne 202");
+            t.Times.Add(new Timer(1, DateTime.Now, DateTime.Today));
+            
+            Model.Projet p2 = new Model.Projet(2, "Projet 2", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam et pretium sapien. Mauris ac justo eros. Nam accumsan ligula nunc, quis iaculis ligula feugiat a. Ut egestas nibh interdum sem porttitor, a vestibulum purus malesuada. Curabitur convallis, felis id mollis rhoncus, urna turpis ornare velit, nec consequat nisi lacus nec neque. Morbi varius faucibus metus sit amet vestibulum. Sed in varius tellus, ac imperdiet diam. Morbi iaculis leo enim, vel mattis risus tempus tincidunt. Fusce vitae felis dolor. Vestibulum rhoncus dapibus nisi vitae sagittis. Sed facilisis sit amet risus eu pharetra.");
+            Task t2 = new Task(2, "Nettoyer et commenter le code");
+            t2.Times.Add(new Timer(1, DateTime.Now, DateTime.MaxValue));
+            
+            p.Tasks.Add(t);
+            p2.Tasks.Add(t2);
+
+            _projets.Add(p);
+            _projets.Add(p2);
             ProfileClick = new Command(ToProfile);
             ProjectClick = new Command(ToProject);
         }
@@ -48,22 +85,25 @@ namespace Projet
         
         public async void ToProject()
         {
+            if (ChosenProject == null) return;
             try
             {
-                Model.Projet p = new Model.Projet(1, "Projet de Test", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam et pretium sapien. Mauris ac justo eros. Nam accumsan ligula nunc, quis iaculis ligula feugiat a. Ut egestas nibh interdum sem porttitor, a vestibulum purus malesuada. Curabitur convallis, felis id mollis rhoncus, urna turpis ornare velit, nec consequat nisi lacus nec neque. Morbi varius faucibus metus sit amet vestibulum. Sed in varius tellus, ac imperdiet diam. Morbi iaculis leo enim, vel mattis risus tempus tincidunt. Fusce vitae felis dolor. Vestibulum rhoncus dapibus nisi vitae sagittis. Sed facilisis sit amet risus eu pharetra.");
-                Task t = new Task(1, "Ecrire le code à la ligne 202");
-                Task t2 = new Task(2, "Nettoyer et commenter le code");
-                t.Times.Add(new Timer(1, DateTime.Now, DateTime.Today));
-                t2.Times.Add(new Timer(1, DateTime.Now, DateTime.MaxValue));
-                p.Tasks.Add(t);
-                p.Tasks.Add(t2);
-                await NavigationService.PushAsync(new ProjectPage(p));
+                ProjectPage projectPage = new ProjectPage(ChosenProject);
+                await NavigationService.PushAsync(projectPage);
             }
-            catch (Exception e)
+            catch(Exception e)
             {
-                Console.WriteLine(e);
+                Console.WriteLine(e.StackTrace);
             }
         }
         
+        public async void DeleteTask()
+        {
+            if (ChosenProject == null) return;
+            bool answer = await App.Current.MainPage.DisplayAlert("Delete", "Do you really want to delete this project?", "Yes", "No");
+            if (!answer) return;
+            Projets.Remove(ChosenProject);
+            ChosenProject = null;
+        }
     }
 }
--- Projet/Projet/ProjectViewModel.cs
+++ Projet/Projet/ProjectViewModel.cs
@@ -1,27 +1,67 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Windows.Input;
 using Projet.Model;
 using Storm.Mvvm;
+using Xamarin.Forms;
 
 namespace Projet
 {
     public class ProjectViewModel : ViewModelBase
     {
+        private Model.Projet _project;
+        private Task _chosenTask;
         private ObservableCollection<Task> _tasks;
+
         public ObservableCollection<Task> Tasks
         {
             get => _tasks;
             set => SetProperty(ref _tasks, value);
         }
+
+        public Task ChosenTask
+        {
+            get => _chosenTask;
+            set => SetProperty(ref _chosenTask, value);
+        }
+
         public Model.Projet Project
         {
-            get;
-            set;
+            get => _project;
+            set => SetProperty(ref _project, value);
         }
+
+        public ICommand TaskClick { get; set; }
+
         public ProjectViewModel(Model.Projet project)
         {
             Project = project;
             Tasks = new ObservableCollection<Task>(project.Tasks);
+            TaskClick = new Command(ToTask);
+        }
+
+        public async void ToTask()
+        {
+            if (ChosenTask == null) return;
+            try
+            {
+                // TaskPage taskPage = new TaskPage(ChosenTask);
+                // await NavigationService.PushAsync(taskPage);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.StackTrace);
+            }
+        }
+        
+        public async void DeleteTask()
+        {
+            if (ChosenTask == null) return;
+            bool answer = await App.Current.MainPage.DisplayAlert("Delete", "Do you really want to delete this task?", "Yes", "No");
+            if (!answer) return;
+            Tasks.Remove(ChosenTask);
+            ChosenTask = null;
         }
     }
 }
